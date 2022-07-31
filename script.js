7d672655bf1b5487bfc460b01713dbd2a8b6982c
Jeremy Hansma <hansmajeremy@gmail.com>
2022-07-31 14:31:22 -0700

    Input from buttons+append, +clear button

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let firstVar = '';
 let secondVar = '';
-
+let shouldResetScreen = false
+let operand = null
 
 const digitButtons = document.querySelectorAll('[data-number]')
 const operandButtons = document.querySelectorAll('[data-operand]')
 const equalsButton = document.querySelector('#equals')
 const pointButton = document.querySelector('#point')
 const clearButton = document.querySelector('#clear')
-const display = document.querySelector('display')
+const display = document.querySelector('.display')
 
 
 subtraction = (a,b) => {
@@ -23,4 +24,28 @@ divide = (a, b) => {
     return a / b
 }
 
+digitButtons.forEach((button) => {
+    button.addEventListener('click', () => appendDigit(button.textContent))
+})
 
+operandButtons.forEach((button) => {
+    button.addEventListener('click', () => operation(button.textContent))
+})
+
+appendDigit = (number) => {
+    if(display.textContent === '0' || shouldResetScreen){
+        resetScreen()
+    }display.textContent += number
+}
+
+resetScreen = () => {
+    display.textContent = '';
+    shouldResetScreen = false;
+}
+
+clear = () => {
+    display.textContent = '0'
+    firstVar = ''
+    secondVar = ''
+    operand = null;
+}
